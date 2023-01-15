62a7e4a3b09ce1030b8d585956d2b31cfcc75e9e
derrick steve kembou nganmoe <kembou.derrick@gmail.com>
2023-01-15 23:19:33 +0100

    add dashobored

---
+++ src/main/java/servlets/accueilServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class accueilServlet
+ */
+@WebServlet("/accueilServlet")
+public class accueilServlet extends HttpServlet {
+	private static final String HOME = "/WEB-INF/dashboard.jsp";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public accueilServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
